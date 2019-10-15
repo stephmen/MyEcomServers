a609149937ca5cd57d0cc544c3664f135ff26c33
stephmen <stephmen@gmail.com>
2019-10-15 18:34:10 -0400

    adding backend change

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,9 +1,10 @@
+//This file connect to the remote prisma server and give us the possibility to query it with JavaScript
 const { Prisma } =require('prisma-binding');
 const db = new Prisma({
-  typeDefs: '../generated/prisma-client/prisma-schema.graphql',
+  typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
-  debug: true
-})
+  debug: true,
+});
 
 module.exports = db
---
+++ Backend/src/resolvers/Mutation.js
@@ -0,0 +1,15 @@
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    console.log(item)
+    return item
+  }
+}
+
+module.exports = Mutation;
