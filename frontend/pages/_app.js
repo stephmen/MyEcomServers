63493bfd04814e5d996a7bab04d2ed19d4473c47
stephmen <stephmen@gmail.com>
2019-10-15 23:25:58 -0400

    appolo client set-up OK

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import App from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData'
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx}) {
+    let pageProps = {};
+    if(Component.getInitialProps){
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    //this expose the query to the users
+    pageProps.quert = ctx.query;
+    return { pageProps }
+  }
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, apollo } = this.props;
     return (
+        <ApolloProvider client={apollo} >
         <Page>
           <Component {...pageProps} />
         </Page>
+        </ApolloProvider>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
